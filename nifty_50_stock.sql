49196566feb23a5fa19c9c144de7e1893b291322
bokade-swaranjali <bokadeswaranjali@gmail.com>
2025-05-18 22:21:26 +0530

    power_bi task-1 completed : create database and power query of all 50 stocks

---
+++ nifty_50_stock.sql
@@ -0,0 +1,598 @@
+CREATE TABLE ADANI_ENT (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from adani_ent;
+
+
+CREATE TABLE ADANI_PORTS (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from ADANI_PORTS;
+
+
+CREATE TABLE ASIAN_PAINT (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from ASIAN_PAINT;
+
+
+CREATE TABLE AXIS_BANK (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from AXIS_BANK;
+
+
+CREATE TABLE BAJAJ_AUTO (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from BAJAJ_AUTO;
+
+
+CREATE TABLE BAJ_FINANCE (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from BAJ_FINANCE;
+
+
+CREATE TABLE BEL (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+
+select * from BEL;
+
+
+CREATE TABLE CIPLA (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from CIPLA;
+
+
+CREATE TABLE COAL_INDIA (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from COAL_INDIA;
+
+
+CREATE TABLE EICHER_MOT (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from EICHER_MOT;
+
+
+CREATE TABLE ETERNAL (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from ETERNAL;
+
+
+CREATE TABLE HDFC_BANK (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from HDFC_BANK;
+
+
+CREATE TABLE HERO_MOTO_CO (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from HERO_MOTO_CO;
+
+
+CREATE TABLE HIND_UNILVR (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+select * from HIND_UNILVR;
+
+
+CREATE TABLE ICICI_BANK (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM ICICI_BANK;
+
+
+CREATE TABLE INDUS_IND_BK (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM INDUS_IND_BK;
+
+
+CREATE TABLE ITC (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM ITC;
+
+
+CREATE TABLE JIO_FIN (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM JIO_FIN;
+
+
+CREATE TABLE KOTAK_BANK (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM KOTAK_BANK;
+
+
+CREATE TABLE LT (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM LT;
+
+
+CREATE TABLE MARUTI (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM MARUTI;
+
+
+CREATE TABLE NESTLE_IND (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM NESTLE_IND;
+
+
+CREATE TABLE NTPC (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM NTPC;
+
+
+CREATE TABLE ONGC (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM ONGC;
+
+CREATE TABLE POWER_GRID (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM POWER_GRID;
+
+
+CREATE TABLE RELIANCE (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM RELIANCE;
+
+
+CREATE TABLE SBIN (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM SBIN;
+
+
+CREATE TABLE TATA_CONSUM (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM TATA_CONSUM;
+
+
+CREATE TABLE TATA_MOTORS (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM TATA_MOTORS;
+
+
+CREATE TABLE TATA_STEEL (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM TATA_STEEL;
+
+
+CREATE TABLE TITAN (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM TITAN;
+
+
+CREATE TABLE TRENT (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM TRENT;
+
+
+CREATE TABLE ULTRA_CEM_CO (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM ULTRA_CEM_CO;
+
+
+CREATE TABLE APOLLO_HOSP (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM APOLLO_HOSP;
+
+
+CREATE TABLE BAJAJ_FINSV (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM BAJAJ_FINSV;
+
+
+CREATE TABLE BHARTI_ARTL (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM BHARTI_ARTL;
+
+
+CREATE TABLE DR_REDDY (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM DR_REDDY;
+
+
+CREATE TABLE GRASIM (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM GRASIM;
+
+
+CREATE TABLE HCL_TECH (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM HCL_TECH;
+
+
+CREATE TABLE HDFC_LIFE (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM HDFC_LIFE;
+
+
+CREATE TABLE HINDALCO (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM HINDALCO;
+
+
+CREATE TABLE INFY (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM INFY;
+
+
+CREATE TABLE JSW_STEEL (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM JSW_STEEL;
+
+
+CREATE TABLE M_M (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM M_M;
+
+
+CREATE TABLE SBI_LIFE (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM SBI_LIFE;
+
+
+CREATE TABLE SHRIRAM_FIN (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM SHRIRAM_FIN;
+
+
+CREATE TABLE SUN_PHARMA (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM SUN_PHARMA;
+
+
+CREATE TABLE TCS (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM TCS;
+
+
+CREATE TABLE TECH_M (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM TECH_M;
+
+
+CREATE TABLE WIPRO (
+    date DATE not null,
+    open DOUBLE PRECISION not null,
+    Ltp double precision not null,
+    volume numeric not null,
+    no_of_trades numeric not null,
+    stock_name TEXT not null
+);
+
+SELECT * FROM WIPRO;
